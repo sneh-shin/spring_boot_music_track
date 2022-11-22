42cad23235daecca7d2823eac17f3fba528e89bd
sneh-shin <snehashinde800@gmail.com>
2022-11-22 18:22:57 +0530

    extended JpaRepository in the TrackRepository interface

--- src/main/java/com/niit/jap/repository/TrackRepository.java
+++ src/main/java/com/niit/jap/repository/TrackRepository.java
@@ -1,4 +1,9 @@
 package com.niit.jap.repository;
 
-public interface TrackRepository {
+import com.niit.jap.domain.Track;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrackRepository extends JpaRepository<Track, Integer> {
 }
