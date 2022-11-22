818f7bd0ddbd903190ce12b42eaed1cd51d9033a
sneh-shin <snehashinde800@gmail.com>
2022-11-22 18:20:31 +0530

    added getters and setters, and toString()

--- src/main/java/com/niit/jap/domain/Track.java
+++ src/main/java/com/niit/jap/domain/Track.java
@@ -27,4 +27,55 @@ public class Track {
         this.trackRating = trackRating;
         this.trackArtist = trackArtist;
     }
+
+    public int getTrackId() {
+        return trackId;
+    }
+
+    public void setTrackId(int trackId) {
+        this.trackId = trackId;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getTrackComments() {
+        return trackComments;
+    }
+
+    public void setTrackComments(String trackComments) {
+        this.trackComments = trackComments;
+    }
+
+    public double getTrackRating() {
+        return trackRating;
+    }
+
+    public void setTrackRating(double trackRating) {
+        this.trackRating = trackRating;
+    }
+
+    public String getTrackArtist() {
+        return trackArtist;
+    }
+
+    public void setTrackArtist(String trackArtist) {
+        this.trackArtist = trackArtist;
+    }
+
+    @Override
+    public String toString() {
+        return "Track{" +
+                "trackId=" + trackId +
+                ", trackName='" + trackName + '\'' +
+                ", trackComments='" + trackComments + '\'' +
+                ", trackRating=" + trackRating +
+                ", trackArtist='" + trackArtist + '\'' +
+                '}';
+    }
 }
