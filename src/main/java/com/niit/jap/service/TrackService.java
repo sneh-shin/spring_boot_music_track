416f027ff9d696f9ca241cdc4480a7339bdb4f76
sneh-shin <snehashinde800@gmail.com>
2022-11-22 18:33:30 +0530

    added abstract methods in the TrackService interface

--- src/main/java/com/niit/jap/service/TrackService.java
+++ src/main/java/com/niit/jap/service/TrackService.java
@@ -1,4 +1,20 @@
 package com.niit.jap.service;
 
+import com.niit.jap.domain.Track;
+
+import java.util.List;
+
 public interface TrackService {
+    Track saveTrack(Track track);
+
+    List<Track> getAllTracks();
+
+    Track getById(int id);
+
+    String deleteTrack(int id);
+
+    List<Track> getByTrackArtist(String trackArtist);
+
+    Track updateTrack(int id);
+
 }
