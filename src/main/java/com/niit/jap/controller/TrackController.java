2f8fbbe7b028f1512db47a8842d6a974875dc199
sneh-shin <snehashinde800@gmail.com>
2022-11-22 18:47:48 +0530

    added object of the TrackServiceImpl.java and injected it in the constructor

--- src/main/java/com/niit/jap/controller/TrackController.java
+++ src/main/java/com/niit/jap/controller/TrackController.java
@@ -5,10 +5,18 @@
  */
 package com.niit.jap.controller;
 
+import com.niit.jap.service.TrackServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class TrackController {
+    private TrackServiceImpl trackService;
+
+    @Autowired
+    public TrackController(TrackServiceImpl trackService) {
+        this.trackService = trackService;
+    }
 }
