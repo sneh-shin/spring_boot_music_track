cd9ed0a1248b41d1ddc1db95cbde53676779642d
sneh-shin <snehashinde800@gmail.com>
2022-11-22 18:37:51 +0530

    added object of the TrackRepository and injected it in the constructor

--- src/main/java/com/niit/jap/service/TrackServiceImpl.java
+++ src/main/java/com/niit/jap/service/TrackServiceImpl.java
@@ -6,12 +6,22 @@
 package com.niit.jap.service;
 
 import com.niit.jap.domain.Track;
+import com.niit.jap.repository.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TrackServiceImpl implements TrackService {
+
+    private TrackRepository trackRepository;
+
+    @Autowired
+    public TrackServiceImpl(TrackRepository trackRepository) {
+        this.trackRepository = trackRepository;
+    }
+
     @Override
     public Track saveTrack(Track track) {
         return null;
