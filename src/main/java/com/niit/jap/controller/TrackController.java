a33e485913fcb48164d6dd794269ee9dbd73a0e7
sneh-shin <snehashinde800@gmail.com>
2022-11-22 18:46:20 +0530

    added @RestController and @RequestMapping annotations

--- src/main/java/com/niit/jap/controller/TrackController.java
+++ src/main/java/com/niit/jap/controller/TrackController.java
@@ -5,5 +5,10 @@
  */
 package com.niit.jap.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class TrackController {
 }
