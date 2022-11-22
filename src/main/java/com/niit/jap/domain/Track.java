ba65cbaad7cd12d8f0d059bc5169cd0831faea47
sneh-shin <snehashinde800@gmail.com>
2022-11-22 18:18:44 +0530

    added attributes in the Track.java

--- src/main/java/com/niit/jap/domain/Track.java
+++ src/main/java/com/niit/jap/domain/Track.java
@@ -5,5 +5,15 @@
  */
 package com.niit.jap.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Track {
+    @Id
+    int trackId;
+    String trackName;
+    String trackComments;
+    double trackRating;
+    String trackArtist;
 }
